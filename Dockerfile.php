0605407f7986b51daac580f4c3595cc5c46cb29a
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2017-03-07 19:57:26 +1000

    Docker - Initiate supervisor and php-fpm on container up

--- Dockerfile.php
+++ Dockerfile.php
@@ -30,7 +30,13 @@ RUN mv composer.phar /usr/local/bin/composer
 # Faster Composer installs
 RUN composer global require "hirak/prestissimo:^0.3"
 
+# Make this container available to others
+EXPOSE 9000
+
 # Install Composer dependencies
 ADD composer-install.sh .
 RUN chmod +x ./composer-install.sh
 CMD ./composer-install.sh
+
+# Start PHP-FPM
+CMD ["php-fpm"]
--- composer-install.sh
+++ composer-install.sh
@@ -2,3 +2,11 @@ until cd /app && composer install
 do
     echo "Retrying Composer install..."
 done
+
+__run_supervisor() {
+    echo "Running the run_supervisor function."
+    supervisord -n
+}
+
+# Call all functions
+__run_supervisor
